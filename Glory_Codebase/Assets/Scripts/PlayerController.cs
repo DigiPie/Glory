ba5e195aba564799a330374734cd1a9e6eb4c51c
Evan <evan.it.sg@gmail.com>
2018-05-22 18:08:06 +0800

    Responsive movement
    Increased all Gravity and Sensitivity values of Horizontal and Vertical input from 3 to 100. Also added slow down force to slow down character if no input is registered and character is still moving.

--- Glory_Codebase/Assets/Scripts/PlayerController.cs
+++ Glory_Codebase/Assets/Scripts/PlayerController.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
-    private Vector2 charVelocity;
+    private Transform groundCheck;
+
     private bool facingRight = true;
     private bool grounded = false;
     private bool jump = false;
-    public float moveForce = 360f;
-    public float dragForce = 90f;
-    public float maxSpeed = 5f;
-    public float jumpForce = 1000f;
-    public Transform groundCheck;
+
+    public float moveForce = 30f; // Since F = ma and m = 1, therefore a = F
+    private float slowdownForce; // Half of moveForce
+    private float slowdownDeadzone = 1.0f; // Do not apply slowdownForce if velocity < this
+    public float maxSpeed = 5f; // Maximum horziontal velocity
+    public float jumpForce = 100f;
 
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        charVelocity = rb2d.velocity;
+        slowdownForce = moveForce * 0.25f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics2D.Linecast(transform.position, 
-            groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        //grounded = Physics2D.Linecast(transform.position, 
+            //groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
         if (Input.GetButtonDown("Jump") && grounded)
         {
@@ -34,32 +36,64 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
+        Move();
+    }
+
+    // Move character
+    void Move()
+    {
+        Debug.Log(rb2d.velocity);
+
         // Get horizontal input for Left/Right movement
-        float h = Input.GetAxis("Horizontal");
-    
-        // If changing direction (h and charVelocity.x have different signs) 
-        // or maxSpeed limit not reached
-        if (h * charVelocity.x < maxSpeed)
+        float inputH = Input.GetAxis("Horizontal");
+        float absVelocityX = Mathf.Abs(rb2d.velocity.x);
+
+        // If there is no horizontal input
+        if (inputH == 0)
+        {
+            // Slow character down until it comes to a complete rest
+            if (absVelocityX > slowdownDeadzone)
+            {
+                // Apply slow down force
+                if (rb2d.velocity.x > 0)
+                {
+                    rb2d.AddForce(Vector2.left * slowdownForce);
+                }
+                else
+                {
+                    rb2d.AddForce(Vector2.right * slowdownForce);
+                }
+            }
+
+            return; // If there is no input, no point calculating movement velocity
+        }
+
+        // If maxSpeed limit not reached
+        if (absVelocityX < maxSpeed)
         {
             // Apply move force
-            rb2d.AddForce(Vector2.right * h * moveForce);
-
-            // If facing changed
-            if (h > 0 && !facingRight || h < 0 && facingRight)
-            {
-                facingRight = !facingRight; // Flip boolean switch
-                Vector3 tempScale = transform.localScale;
-                tempScale.x *= -1;
-                transform.localScale = tempScale; // Flip scale on x-axis
-            }
+            rb2d.AddForce(Vector2.right * inputH * moveForce);
+            UpdateFacing(inputH);
         }
 
-        // If charVelocity.x > maxSpeed limit
-        if (Mathf.Abs(charVelocity.x) > maxSpeed)
+        absVelocityX = Mathf.Abs(rb2d.velocity.x); // Update to check if > maxSpeed limit
+
+        // If rb2d.velocity.x > maxSpeed limit
+        if (absVelocityX > maxSpeed)
         {
-            // Set charVelocity to max velocity based on maxSpeed limit
-            charVelocity = new Vector2(Mathf.Sign(charVelocity.x) * maxSpeed, charVelocity.y);
+            // Set rb2d.velocity to max velocity based on maxSpeed limit
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
         }
-       
     }
+
+    void UpdateFacing(float inputH) {
+        // If facing changed
+        if (inputH > 0 && !facingRight || inputH < 0 && facingRight)
+        {
+            facingRight = !facingRight; // Flip boolean switch
+            Vector3 tempScale = transform.localScale;
+            tempScale.x *= -1;
+            transform.localScale = tempScale; // Flip scale on x-axis
+        }
+    }  
 }
