f4599be65a62327e6c34564b68b9e796b4bde752
Evan <evan.it.sg@gmail.com>
2018-05-22 23:08:04 +0800

    Jumping and Wall collision handling
    Implemented jumping and also a slight bounce-off effect when colliding with walls. This is to prevent the character from getting stuck against the wall and not falling when mid-air.

--- Glory_Codebase/Assets/Scripts/PlayerController.cs
+++ Glory_Codebase/Assets/Scripts/PlayerController.cs
@@ -6,54 +6,88 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
     private Transform groundCheck;
 
+    private bool againstWall = false;
+    private bool collisionOnRight;
     private bool facingRight = true;
-    private bool grounded = false;
-    private bool jump = false;
-
-    public float moveForce = 30f; // Since F = ma and m = 1, therefore a = F
-    private float slowdownForce; // Half of moveForce
-    private float slowdownDeadzone = 1.0f; // Do not apply slowdownForce if velocity < this
+    private bool onGround = false;
+    
+    public float moveForce = 50f; // Since F = ma and m = 1, therefore a = F
     public float maxSpeed = 5f; // Maximum horziontal velocity
-    public float jumpForce = 100f;
+    public float jumpForce = 500f;
 
+    private float jumpMoveForce; // Half of moveForce, slower horizontal speed in the air
+    private float slowdownForce; // Quarter of moveForce, applied on character when no input
+    private readonly float deadzoneFactor = 1.0f; // Do not apply certain forces if velocity < or > this
+    
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         rb2d = GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("groundCheck");
+        jumpMoveForce = moveForce * 0.5f;
         slowdownForce = moveForce * 0.25f;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame, independent of the physics engine
     void Update()
     {
-        //grounded = Physics2D.Linecast(transform.position, 
-            //groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
 
-        if (Input.GetButtonDown("Jump") && grounded)
+    // Update is called in-step with the physics engine
+    void FixedUpdate ()
+    {
+        Move();
+        Jump();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If colliding with wall
+        if (collision.gameObject.layer == 9)
         {
-            jump = true;
+            againstWall = true;
+            collisionOnRight = collision.contacts[0].point.x > transform.position.x;
         }
     }
 
-    // Update is called once per frame
-    void FixedUpdate () {
-        Move();
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // If colliding with wall
+        if (collision.gameObject.layer == 9)
+        {
+            againstWall = false;
+        }
     }
 
     // Move character
     void Move()
     {
-        Debug.Log(rb2d.velocity);
-
         // Get horizontal input for Left/Right movement
         float inputH = Input.GetAxis("Horizontal");
         float absVelocityX = Mathf.Abs(rb2d.velocity.x);
+
+        // If against wall
+        if (againstWall)
+        {
+            if (collisionOnRight)
+            {
+                rb2d.AddForce(Vector2.left * moveForce);
+            }
+            else
+            {
+                rb2d.AddForce(Vector2.right * moveForce);
+            }
+
+            return; // Disallow user-movement
+        }
 
         // If there is no horizontal input
         if (inputH == 0)
         {
             // Slow character down until it comes to a complete rest
-            if (absVelocityX > slowdownDeadzone)
+            if (absVelocityX > deadzoneFactor)
             {
+                // Do not apply slow down force if velocity is smaller than deadzoneFactor
                 // Apply slow down force
                 if (rb2d.velocity.x > 0)
                 {
@@ -72,8 +106,16 @@ public class PlayerController : MonoBehaviour {
         if (absVelocityX < maxSpeed)
         {
             // Apply move force
-            rb2d.AddForce(Vector2.right * inputH * moveForce);
             UpdateFacing(inputH);
+
+            if (onGround)
+            {
+                rb2d.AddForce(Vector2.right * inputH * moveForce);
+            }
+            else
+            {
+                rb2d.AddForce(Vector2.right * inputH * jumpMoveForce);
+            }
         }
 
         absVelocityX = Mathf.Abs(rb2d.velocity.x); // Update to check if > maxSpeed limit
@@ -86,7 +128,24 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void UpdateFacing(float inputH) {
+    // Jump
+    void Jump()
+    {
+        onGround = Physics2D.Linecast(transform.position, groundCheck.position,
+            1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButton("Jump"))
+        {
+            // Only apply if y velocity is smaller than deadzoneFactor
+            if (onGround && Mathf.Abs(rb2d.velocity.y) < deadzoneFactor) 
+            {
+                rb2d.AddForce(new Vector2(0f, jumpForce));
+            }
+        }
+    }
+
+    void UpdateFacing(float inputH)
+    {
         // If facing changed
         if (inputH > 0 && !facingRight || inputH < 0 && facingRight)
         {
