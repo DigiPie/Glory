3c0f029a3a63f82c22de0ce2a586a9907c462a18
NorthKoreaIsBestKorea <35793588+NorthKoreaIsBestKorea@users.noreply.github.com>
2018-05-23 15:08:25 +0800

    Enemy and Health Bar

---
+++ Glory_Codebase/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour {
+    private HealthSystem healthSystem;
+
+	// Use this for initialization
+	void Start () {
+		healthSystem = GetComponent<HealthSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If colliding with player
+        if (collision.gameObject.layer == 11)
+        {
+            healthSystem.DeductHealth(10);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+}
---
+++ Glory_Codebase/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour {
+    private Transform healthBar;
+    private Renderer healthBarR, healthBarBackR;
+
+    public float startingHealth = 100;
+    private float currentHealth;
+
+    // Use this for initialization
+    void Start () {
+        healthBar = transform.Find("Health_Enemy_1_red");
+        healthBarR = healthBar.GetComponent<Renderer>();
+        healthBarBackR = transform.Find("Health_Enemy_1_black").GetComponent<Renderer>();
+        currentHealth = startingHealth;
+
+        healthBarR.enabled = false;
+        healthBarBackR.enabled = false;
+    }
+
+    public void DeductHealth(float damage)
+    {
+        currentHealth -= damage;
+        SetHealthBarScale(currentHealth / startingHealth);
+
+        healthBarR.enabled = true;
+        healthBarBackR.enabled = true;
+    }
+
+    private void SetHealthBarScale(float scale)
+    {
+        Vector3 tempScale = healthBar.localScale;
+        tempScale.x *= scale;
+        healthBar.localScale = tempScale;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
