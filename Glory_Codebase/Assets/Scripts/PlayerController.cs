62c04fef583f07a837d80f4ecd820300785e7f2b
Evan <evan.it.sg@gmail.com>
2018-05-26 19:51:27 +0800

    PlayerController script and Animator optimisation
    Cleaned up and optimised the PlayerController script. Using Animator parameters to animate instead of animator.Play() now. Implemented proper Animator states and transitions.

--- Glory_Codebase/Assets/Scripts/PlayerController.cs
+++ Glory_Codebase/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,27 @@ public class PlayerController : MonoBehaviour {
     private SpriteRenderer sprite;
     private Transform groundCheck;
 
+    // Forces to be applied on character
+    private Vector2 bounceWallLeftV, bounceWallRightV;
+    private Vector2 bounceEnemyLeftV, bounceEnemyRightV;
+    private Vector2 slowdownLeft, slowdownRightV;
+    private Vector2 moveLeftV, moveRightV, moveLeftInAirV, moveRightInAirV;
+    private Vector2 jumpV;
+
     private bool againstWall = false;
     private bool againstEnemy = false;
     private bool collisionOnRight = false;
     private bool onGround = false;
-    
+
+    private bool inputJump;
+    private float inputH;
+
     public float moveForce = 50f; // Since F = ma and m = 1, therefore a = F
     public float maxSpeed = 5f; // Maximum horziontal velocity
     public float jumpForce = 500f;
     public float throwbackForce = 200f; // When hit by enemy
 
-    private float jumpMoveForce; // Half of moveForce, slower horizontal speed in the air
+    private float inAirMoveForce; // Half of moveForce, slower horizontal speed in the air
     private float slowdownForce; // Quarter of moveForce, applied on character when no input
     private readonly float deadzoneFactor = 1.0f; // Do not apply certain forces if velocity < or > this
     
@@ -29,8 +39,23 @@ public class PlayerController : MonoBehaviour {
         rb2d = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         groundCheck = transform.Find("groundCheck");
-        jumpMoveForce = moveForce * 0.5f;
+
+        inAirMoveForce = moveForce * 0.5f;
         slowdownForce = moveForce * 0.25f;
+
+        // Calculate the bounce-off vectors here instead of FixedUpdate() so we only
+        // calculate them once, as they never change. For optimisation.
+        bounceWallLeftV = Vector2.right * moveForce;
+        bounceWallRightV = Vector2.left * moveForce;
+        bounceEnemyLeftV = new Vector2(1, 0.5f) * throwbackForce;
+        bounceEnemyRightV = new Vector2(-1, 0.5f) * throwbackForce;
+        slowdownLeft = Vector2.left * slowdownForce;
+        slowdownRightV = Vector2.right * slowdownForce;
+        moveLeftV = Vector2.left * moveForce;
+        moveRightV = Vector2.right * moveForce;
+        moveLeftInAirV = Vector2.left * inAirMoveForce;
+        moveRightInAirV = Vector2.right * inAirMoveForce;
+        jumpV = new Vector2(0f, jumpForce);
     }
 
     // Update is called once per frame, independent of the physics engine
@@ -42,7 +67,6 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate ()
     {
         Move();
-        Jump();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -78,118 +102,126 @@ public class PlayerController : MonoBehaviour {
     // Move character
     void Move()
     {
+        // Update physics information
         onGround = Physics2D.Linecast(transform.position, groundCheck.position,
             1 << LayerMask.NameToLayer("Ground"));
 
-        // Get horizontal input for Left/Right movement
-        float inputH = Input.GetAxis("Horizontal");
-        float absVelocityX = Mathf.Abs(rb2d.velocity.x);
+        // Update input information
+        inputH = Input.GetAxis("Horizontal");
+        inputJump = Input.GetButton("Jump");
 
+        // Apply bounce-off forces when colliding with wall and enemies.
+        HandleBounceOff();
+
+        if (inputH == 0)
+        {
+            // If there is no horizontal input
+            HandleSlowdown(); // Slow down velocity on the x-axis
+            animator.SetBool("Running", false);
+        }
+        else
+        {
+            // If there is horizontal input
+            HandleRunning();
+            animator.SetBool("Running", true);
+            sprite.flipX = inputH < 0;
+        }
+
+        if(inputJump)
+        {
+            HandleJumping();
+        }
+
+        animator.SetBool("Jumping", !onGround);
+    }
+
+    // Apply bounce-off forces when colliding with wall and enemies.
+    bool HandleBounceOff()
+    {
         // If against wall
         if (againstWall)
         {
             if (collisionOnRight)
             {
-                rb2d.AddForce(Vector2.left * moveForce);
+                rb2d.AddForce(bounceWallRightV);
             }
             else
             {
-                rb2d.AddForce(Vector2.right * moveForce);
+                rb2d.AddForce(bounceWallLeftV);
             }
-
-            return; // Disallow user-movement
         }
 
         if (againstEnemy)
         {
             if (collisionOnRight)
             {
-                rb2d.AddForce(new Vector2(-1, 0.5f) * throwbackForce);
+                rb2d.AddForce(bounceEnemyRightV);
             }
             else
             {
-                rb2d.AddForce(new Vector2(1, 0.5f) * throwbackForce);
+                rb2d.AddForce(bounceEnemyLeftV);
             }
-
-            return; // Disallow user-movement
         }
 
-        // If there is no horizontal input
-        if (inputH == 0)
-        {
-            // Slow character down until it comes to a complete rest
-            if (absVelocityX > deadzoneFactor)
-            {
-                // Do not apply slow down force if velocity is smaller than deadzoneFactor
-                // Apply slow down force
-                if (rb2d.velocity.x > 0)
-                {
-                    rb2d.AddForce(Vector2.left * slowdownForce);
-                }
-                else
-                {
-                    rb2d.AddForce(Vector2.right * slowdownForce);
-                }
-            }
+        // Return true if bouncing off either against wall or enemy
+        return againstWall || againstEnemy;
+    }
 
-            if (onGround)
+    // Apply slowdown forces when no horizontal input is registered.
+    void HandleSlowdown()
+    {
+        // Apply slow down force if velocity is significant.
+        if (Mathf.Abs(rb2d.velocity.x) > deadzoneFactor)
+        {
+            if (rb2d.velocity.x > 0)
             {
-                animator.Play("Idle");
+                rb2d.AddForce(slowdownLeft);
             }
             else
             {
-                animator.Play("Jump");
+                rb2d.AddForce(slowdownRightV);
             }
-
-            return; // If there is no input, no point calculating movement velocity
-        }
-
-        if (onGround)
-        {
-            animator.Play("Run");
-        }
-        else
-        {
-            animator.Play("Jump");
-        }
-
-        // If maxSpeed limit not reached
-        if (absVelocityX < maxSpeed)
-        {
-            // Update facing
-            sprite.flipX = inputH < 0;
-
-            // Apply move force
-            if (onGround)
-            {
-                rb2d.AddForce(Vector2.right * inputH * moveForce);
-            }
-            else
-            {
-                rb2d.AddForce(Vector2.right * inputH * jumpMoveForce);
-            }
-        }
-
-        absVelocityX = Mathf.Abs(rb2d.velocity.x); // Update to check if > maxSpeed limit
-
-        // If rb2d.velocity.x > maxSpeed limit
-        if (absVelocityX > maxSpeed)
-        {
-            // Set rb2d.velocity to max velocity based on maxSpeed limit
-            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
         }
     }
 
-    // Jump
-    void Jump()
+    // Apply horizontal movement forces if horizontal input is registered.
+    void HandleRunning()
     {
-        if (Input.GetButton("Jump"))
+        // Apply forces if max speed is not yet reached
+        if (Mathf.Abs(rb2d.velocity.x) < maxSpeed)
         {
-            // Only apply if y velocity is smaller than deadzoneFactor
-            if (onGround && Mathf.Abs(rb2d.velocity.y) < deadzoneFactor) 
+            if (inputH < 0)
             {
-                rb2d.AddForce(new Vector2(0f, jumpForce));
+                if (onGround)
+                {
+                    rb2d.AddForce(moveLeftV);
+                }
+                else
+                {
+                    rb2d.AddForce(moveLeftInAirV);
+                }
             }
+            else
+            {
+                if (onGround)
+                {
+                    rb2d.AddForce(moveRightV);
+                }
+                else
+                {
+                    rb2d.AddForce(moveRightV);
+                }
+            }
+        }
+    }
+
+    // Apply vertical movement force if jump input is registered.
+    void HandleJumping()
+    {
+        // Apply force if on ground and y velocity is insignificant.
+        if (onGround && Mathf.Abs(rb2d.velocity.y) < deadzoneFactor)
+        {
+            rb2d.AddForce(jumpV);
         }
     }
 }
